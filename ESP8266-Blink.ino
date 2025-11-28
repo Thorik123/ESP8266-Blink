a3faad2a5e9fa831b89370cc30696dc1faeebf20
Thorik123 <tronikthorik07@gmail.com>
2025-11-28 20:18:49 +0700

    Add button to enable/disable blink

--- ESP8266-Blink.ino
+++ ESP8266-Blink.ino
@@ -1,13 +1,30 @@
 
 const int LED_PIN = 2;
+const int BUTTON_PIN = 0;
+
+bool enableBlink = false;
 
 void setup()
 {
     pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop()
 {
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    delay(500);
+    if (digitalRead(BUTTON_PIN) == LOW)
+    {
+        enableBlink = !enableBlink;
+        delay(300); // Debounce delay
+    }
+
+    if (enableBlink)
+    {
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        delay(500);
+    }
+    else
+    {
+        digitalWrite(LED_PIN, LOW);
+    }
 }
